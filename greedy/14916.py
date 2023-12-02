dea8795af0b42df04e85d22482148092bf0394e0
SehwanChang <kdwkdwkd@naver.com>
2023-12-02 23:51:56 +0900

    logic fixed

---
+++ greedy/14916.py
@@ -0,0 +1,18 @@
+# import sys
+# input = sys.stdin.readline
+n = int(input())
+cnt = 0
+while True :
+    if n % 5 == 0 :
+        cnt += n // 5
+        break
+    else : 
+        n -= 2
+        cnt += 1
+    
+    if n < 0 :
+        break
+if n < 0 : 
+    print(-1)
+else : 
+    print(cnt)
