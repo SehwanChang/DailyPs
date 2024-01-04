cfc7235af505dea08aa85c0ecdd235c9ea9331c2
SehwanChang <kdwkdwkd@naver.com>
2024-01-04 16:30:32 +0900

    bfs gold 5

--- bfs/7569.py
+++ bfs/7569.py
@@ -9,15 +9,16 @@ for i in range(h):
         for k in range(m):
             if graph[i][j][k] == 1:
                 q.append((i, j, k, 0))
+dz = [0, 0, 0, 0, 1, -1]
 dx = [1, -1, 0, 0, 0, 0]
 dy = [0, 0, 1, -1, 0, 0]
-dz = [0, 0, 0, 0, 1, -1]
+
 while q :
     z, x, y, time = q.popleft()
     for i in range(6) :
         nz = z + dz[i]
-        ny = y + dy[i]
         nx = x + dx[i]
+        ny = y + dy[i]
         if 0 <= nz < h and 0 <= nx < n and 0 <= ny < m :
             if graph[nz][nx][ny] == 0 :
                 graph[nz][nx][ny] = 1
