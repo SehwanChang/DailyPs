1675193f74b57c1eb41d2d0c5545ab52d9b6b173
SehwanChang <kdwkdwkd@naver.com>
2024-01-16 14:19:07 +0900

    stack gold 2

---
+++ stack/1918.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+infix = list(input())
+stack=[]
+res=''
+for s in infix:
+    if s.isalpha() :
+        res += s
+    else :
+        if s == '(' :
+            stack.append(s)
+        elif s == '*' or s == '/' :
+            while stack and (stack[-1] == '*' or stack[-1] == '/') :
+                res += stack.pop()
+            stack.append(s)
+        elif s ==  '+' or s == '-' :
+            while stack and stack[-1] != '(' :
+                res += stack.pop()
+            stack.append(s)
+        elif s == ')' :
+            while stack and stack[-1] != '(' :
+                res += stack.pop()
+            stack.pop()
+while stack :
+    res+=stack.pop()
+print(res)
