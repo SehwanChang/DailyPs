81c27bb680bed4141faa9c7ae726f8f0fd70bb92
SehwanChang <kdwkdwkd@naver.com>
2024-01-17 17:30:45 +0900

    heap

---
+++ heap/11286.py
@@ -0,0 +1,14 @@
+import sys
+import heapq
+input = sys.stdin.readline
+heap = []
+n = int(input())
+for _ in range(n) :
+    num = int(input())
+    if num :
+        heapq.heappush(heap, (abs(num), num))
+    else :
+        if heap :
+            print(heapq.heappop(heap)[1])
+        else :
+            print(0)
