27d72d78e9e0422c3af7c2d275aa0e747499026f
SehwanChang <kdwkdwkd@naver.com>
2024-02-02 14:45:03 +0900

    backtracking silver 2

--- back_tracking/15665.py
+++ back_tracking/15665.py
@@ -3,13 +3,17 @@ input = sys.stdin.readline
 n, m = map(int, input().split())
 nums = list(map(int, input().split()))
 nums.sort()
-answer = []
-def backtracking(level, idx) :
-    if level == m:
-        print(*answer)
+ans = []
+ans_set = set()
+def backtracking(idx) :
+    if len(ans) == m:
+        ans_tuple = tuple(ans)
+        if ans_tuple not in ans_set :
+            print(*ans)
+            ans_set.add(ans_tuple)
         return
     for i in range(idx, n) :
-        answer.append(nums[i])
-        backtracking(level + 1 , idx + 1)
-        answer.pop()
-backtracking(0, 0)
+        ans.append(nums[i])
+        backtracking(idx)
+        ans.pop()
+backtracking(0)
