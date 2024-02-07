25c778fcb0d7e8743cdfdf6d96d033e13695bc56
SehwanChang <kdwkdwkd@naver.com>
2024-02-07 13:31:08 +0900

    dfs refactoring

--- dfs/2667.py
+++ dfs/2667.py
@@ -1,11 +1,7 @@
 n = int(input())
-graph = []
-for i in range(n) :
-    data = list(map(int, input()))
-    graph.append(data)
-
+graph = [list(map(int, input())) for _ in range(n)]
 #북 동 남 서
-dx = [-1 ,0, 1, 0]
+dx = [1 ,0, -1, 0]
 dy = [0, 1, 0, -1]
 
 cnt = 0
@@ -13,15 +9,15 @@ result = []
 
 def dfs(x, y) :
     global cnt
-    if x < 0 or x >= n or y < 0 or y >= n :
-        return
-    if graph[x][y] == 1 :
-        cnt += 1
-        graph[x][y] = 0
-        for i in range(4) :
-            nx = x + dx[i]
-            ny = y + dy[i]
-            dfs(nx, ny)
+    if 0 <= x < n and 0 <= y < n :
+        if graph[x][y] == 1 :
+            cnt += 1
+            graph[x][y] = 0
+            for i in range(4) :
+                nx = x + dx[i] 
+                ny = y + dy[i]
+                dfs(nx, ny) 
+         
 for i in range(n) :
     for j in range(n) :
         if graph[i][j] == 1 :
