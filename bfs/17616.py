5706d61b0bf77dbff9a6e6831c2adc162657cd0e
SehwanChang <kdwkdwkd@naver.com>
2024-03-04 16:57:24 +0900

    code changeD

--- bfs/17616.py
+++ bfs/17616.py
@@ -4,10 +4,6 @@ input = sys.stdin.readline
 n, m , x = map(int, input().split())
 higher = [[] for _ in range(n + 1)]
 lower = [[] for _ in range(n + 1)]
-for _ in range(m) :
-    a, b = map(int, input().split())
-    lower[a].append(b)
-    higher[b].append(a)
 
 def bfs(start, graph) :
     q = deque()
@@ -24,34 +20,13 @@ def bfs(start, graph) :
                 q.append(next)
 
     return rank
+
+for _ in range(m) :
+    a, b = map(int, input().split())
+    lower[a].append(b)
+    higher[b].append(a)
+
 high_rank = 1 + bfs(x , higher)
 low_rank = n - bfs(x, lower)
 
 print(high_rank, low_rank)
-
-# 5 3 1
-# 2 3
-# 3 4
-# 4 5
-
-# 1번 노드 직접적으로 달성 x : 역방향 추적.
-
-# 2 > 3 > 4 > 5 : 1 or 5 가능함.
-
-# 5 5 1
-# 1 3
-# 2 3
-# 3 4
-# 3 5
-# 4 5
-
-# 3 > 4 > 5 : 1 or 2 가능함
-
-
-# 5 3 1
-# 2 4
-# 3 1
-# 2 3
-# 5 2 3 4 1
-# 2 3 1 4
-# 2 3 4 1
