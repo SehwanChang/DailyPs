9cba953caa94a3b15f49e1509c77364b0cf8cd45
SehwanChang <kdwkdwkd@naver.com>
2024-03-31 00:20:32 +0900

    bfs silver 2 code changed

--- bfs/13565.py
+++ bfs/13565.py
@@ -6,20 +6,17 @@ graph = [list(map(int, input().strip())) for _ in range(m)]
 dx = [1, 0, -1, 0]
 dy = [0, 1, 0, -1]
 q = deque()
-for i in range(m) :
-    for j in range(n) :
-        if graph[i][j] == 1 :
-            q.append((i, j))
+for i in range(n) :
+    if graph[0][i] == 0 :
+        q.append((0, i))
+
 while q :
     x, y = q.popleft()
+    graph[x][y] = 2
     for i in range(4) :
         nx = x + dx[i]
         ny = y + dy[i]
-        if 0 <= nx < m and 0 <= ny < n and graph[nx][ny] == 1:
-            q.append((nx, ny))
+        if 0 <= nx < m and 0 <= ny < n and graph[nx][ny] == 0:
             graph[nx][ny] = 2
-print(graph)
-if 2 in graph[m - 1] :
-    print('YES')
-else :
-    print('NO')
+            q.append((nx, ny))
+print('YES') if 2 in graph[-1] else print('NO')
