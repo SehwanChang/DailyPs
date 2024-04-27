5ead7ae55230625f3da5c8baefac442cf0f488ea
SehwanChang <kdwkdwkd@naver.com>
2024-04-27 23:43:50 +0900

    [backtracking] silver 1

---
+++ back_tracking/16198.py
@@ -0,0 +1,18 @@
+import sys
+def back_tacking(x):
+    global answer
+    if len(w) == 2:
+        answer = max(answer, x)
+        return
+    for i in range(1, len(w) - 1):
+        target = w[i - 1] * w[i + 1] 
+        v = w.pop(i)
+        back_tacking(x + target) 
+        w.insert(i, v) 
+
+
+n = int(sys.stdin.readline())
+w = list(map(int, sys.stdin.readline().split()))
+answer = 0
+back_tacking(0)
+print(answer)
