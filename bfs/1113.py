79cf358bddd4fc3a05f1479246e2191b9aac10d1
SehwanChang <kdwkdwkd@naver.com>
2024-05-02 22:52:58 +0900

    [bfs] gold 1

---
+++ bfs/1113.py
@@ -0,0 +1,44 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+n, m = map(int, input().split())
+graph = [list(map(int, input().strip())) for _ in range(n)]
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
+water = 0
+
+
+def bfs(x, y):
+    global water
+    q = deque()
+    q.append((x, y))
+    visited = [[False] * m for _ in range(n)]
+    height = graph[x][y]
+    min_height = 10
+    mini_pool = [(x, y)]
+    visited[x][y] = True
+
+    while q:
+        x, y = q.popleft()
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+            if nx in (-1, n) or ny in (-1, m):
+                return 0, 0
+            if height >= graph[nx][ny] and not visited[nx][ny]:
+                q.append((nx, ny))
+                visited[nx][ny] = True
+                mini_pool.append((nx, ny))
+
+            elif height < graph[nx][ny]:
+                min_height = min(min_height, graph[nx][ny])
+    for pos in mini_pool:
+        x, y = pos
+        water += (min_height - graph[x][y])
+        graph[x][y] = min_height
+
+
+for i in range(1, n - 1):
+    for j in range(1, m - 1):
+        bfs(i, j)
+
+print(water)
