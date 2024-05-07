1f009abc40817ead2865934b64d15d94842f1796
SehwanChang <kdwkdwkd@naver.com>
2024-05-07 20:07:10 +0900

    [구현] silver 3

--- 구현/1913.py
+++ 구현/1913.py
@@ -2,7 +2,7 @@ import sys
 input = sys.stdin.readline
 n = int(input())
 target = int(input())
-graph = [[0] * n for _ in range(n)]
+graph = [[-1] * n for _ in range(n)]
 
 #  0:아래, 1:오른쪽, 2:위, 3:왼쪽
 dx = [1, 0, -1, 0]
@@ -14,18 +14,21 @@ num = n * n
 ans = [0, 0]
 while num > 1:
     if num == target:
-        ans[0] = pos_x + 1
-        ans[1] = pos_y + 1
+        ans = [pos_x + 1, pos_y + 1]
+        # print('여기', ans[0], ans[1])
+        # num -= 1
     graph[pos_x][pos_y] = num
     nx, ny = pos_x + dx[dir], pos_y + dy[dir]
-    if 0 <= nx < n and 0 <= ny < n and graph[nx][ny] == 0:
+    if 0 <= nx < n and 0 <= ny < n and graph[nx][ny] == -1:
         pos_x, pos_y = nx, ny
         num -= 1
     else:
         dir = (dir + 1) % 4
-
-graph[(n - 1) // 2][(n - 1) // 2] = 1
+graph[pos_x][pos_y] = num
 for row in graph:
     print(*row)
 
-print(*ans)
+if target == 1:
+    print(pos_x + 1, pos_y + 1)
+else:
+    print(*ans)
